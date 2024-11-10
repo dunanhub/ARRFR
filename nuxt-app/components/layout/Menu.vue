9e9f92de9233ea9c6f29a28dc98c05e488bb42a3
Beka <ubniyev.b@gmail.com>
2024-11-10 17:44:26 +0500

    WIN

--- nuxt-app/components/layout/Menu.vue
+++ nuxt-app/components/layout/Menu.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="mr-3">
         <NuxtLink to="/" class="flex items-center py-1 px-3 rounded-r-lg w-full h-full hover:bg-[#4c4470] hover:shadow transition-all">
-            <Icon name="humbleicons:dashboard" class="mr-3"/>
-            <span>Home</span>
+            <Icon name="humbleicons:dashboard" size="1.2em" class="mr-3"/>
+            <span>Главная</span>
+        </NuxtLink>
+        <NuxtLink to="/" class="flex items-center py-1 px-3 my-2 rounded-r-lg w-full h-full hover:bg-[#4c4470] hover:shadow transition-all">
+            <Icon name="line-md:email" size="1.2em" class="mr-3"/>
+            <span>Почта</span>
+        </NuxtLink>
+        <NuxtLink to="/" class="flex items-center py-1 px-3 my-2 rounded-r-lg w-full h-full hover:bg-[#4c4470] hover:shadow transition-all">
+            <Icon name="cil:chart-line" size="1.2em" class="mr-3"/>
+            <span>Статистика</span>
+        </NuxtLink>
+        <NuxtLink to="/" class="flex items-center py-1 px-3 my-2 rounded-r-lg w-full h-full hover:bg-[#4c4470] hover:shadow transition-all">
+            <Icon name="uim:analytics" size="1.2em" class="mr-3"/>
+            <span>Аналитика</span>
         </NuxtLink>
     </div>
 </template>
---
+++ nuxt-app/components/layout/Search.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <NuxtLink to="#" class="flex justify-center w-200 py-4 px-8">
+            <Icon name="line-md:search" size="1.2em" />
+            <span>Search</span>
+        </NuxtLink>
+    </div>
+</template>
