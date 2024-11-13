24f83f785fab38726efd11d02a72864b2e5e2f6a
Beka <ubniyev.b@gmail.com>
2024-11-13 23:18:20 +0500

    add login block

--- nuxt-app/components/layout/Header.vue
+++ nuxt-app/components/layout/Header.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex justify-between bg-[var(--bg-nav-color)] px-20">
+    <div class="flex justify-between items-center bg-[var(--bg-nav-color)] px-20">
         <LayoutLogo />
         <LayoutNavbar />
+        <LayoutLogin />
     </div>
 </template>
